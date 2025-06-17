163b872986dd16a352a9c58b08b49f330f9f58b7
IgorRRamos <ramosigorprofissional@gmail.com>
2025-06-16 23:06:13 -0300

    Atualização codigo beecrowd-1847 adicionado funcão para verificação dos emojis

--- Logica/beecrowd-1435/src/Main.java
+++ Logica/beecrowd-1435/src/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         int x;
-        int n = entrada.nextInt();
+        int n;
+        n = entrada.nextInt();
         while(n != 0){
             for (int i = 1; i <= n; i++){
                 for (int j = 1; j <= n; j++){
--- Logica/beecrowd-1847/src/Main.java
+++ Logica/beecrowd-1847/src/Main.java
@@ -1,6 +1,40 @@
 import java.util.Scanner;
 
+/*
+    Quais são os dados de entrada?:
+        Sequencia de 3 numeros inteiros representando temperaturas!
+
+    Oque devo fazer com esses dados?:
+        Verificar as variações entre eles para retornar o emoji correto referente
+        as mudanças de temperatura.
+
+    Quais são as restrições do problema?:
+        Não ter retorno de emojis incorretos
+
+    Qual é o resultado esperado?:
+        Emojis especificos pertinentes a cada variação na temperatura
+
+    Qual é a sequencia de passos a ser feita para chegar no resultado esperado?:
+        Ler os 3 numeros inteiros de entrada!
+        verificar as variações entre eles!
+        Escrever na tela o emoji feliz caso a variação seja boa
+        Escrever na tela o emoji triste caso a viriação seja ruim
+ */
+
+
 public class Main {
+
+        public static void happyEmoji(int x, int y, int z){
+
+            boolean happy = x > y && y <= z || x < y && y < z && y - x < z - y || x > y && x - y > y - z || x == y && y < z;
+            if(happy){
+                System.out.println(":)");
+            }else{
+                System.out.println(":(");
+            }
+
+        }
+
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
@@ -10,19 +44,9 @@ public class Main {
         b = entrada.nextInt();
         c = entrada.nextInt();
 
-        if (b < a && c >= b) {
-            System.out.println(":)");
-        } else if (b > a && c > b && b - c < a - b) {
-            System.out.println(":)");
-        } else if (b < a && c < b && a - b > b - c) {
-            System.out.println(":)");
-        } else if (a == b && c > b) {
-            System.out.println(":)");
-        } else {
-            System.out.println(":(");
-        }
-
+        happyEmoji(a, b, c);
 
         entrada.close();
     }
 }
+
