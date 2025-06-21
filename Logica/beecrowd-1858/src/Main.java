fb3dc3546f731ac4ebd86fb7bdd21360bcef26d5
IgorRRamos <ramosigorprofissional@gmail.com>
2025-06-21 18:38:56 -0300

    beecrowd 1858 resolução.

--- Logica/beecrowd-1858/src/Main.java
+++ Logica/beecrowd-1858/src/Main.java
@@ -5,27 +5,35 @@ public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
-        while (entrada.hasNextLine()) {
-            int N, T, cont = 0, verificacao=0;
+        int N, T, cont = 0, verificacao = 0;
 
-            N = entrada.nextInt();
-            int[] pessoa = new int[N];
-            for (int i = 1; i <= N; i++) {
-                pessoa[i - 1] = entrada.nextInt();
+        N = entrada.nextInt();
+        int[] pessoa = new int[N];
+        for (int i = 1; i <= N; i++) {
+            pessoa[i - 1] = entrada.nextInt();
+
+
+
+        }
+        verificacao = pessoa[0];
+
+        for (int i = 0; i < N; i++) {
+            if (pessoa[i] < verificacao) {
+                verificacao = pessoa[i];
             }
-            verificacao = pessoa[0];
+        }
 
-            for(int i=0;i<N;i++){
-                if(pessoa[i - 1] < verificacao){
-                    verificacao = pessoa[i - 1];
-                    cont=i;
-                }
-
+        for (int i = 0; i < N; i++) {
+            if (pessoa[i] <= verificacao) {
+                cont = i+1;
+                break;
             }
-            System.out.println(cont);
         }
 
 
+
+
+        System.out.println(cont);
 
 
         entrada.close();
