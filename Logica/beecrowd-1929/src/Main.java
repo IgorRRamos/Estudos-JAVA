6f7f3524462e1aab2f9e38931a3d1b29d41430f2
IgorRRamos <ramosigorprofissional@gmail.com>
2025-06-26 22:37:40 -0300

    Beecrowd-1929 incompleto Wrong answer (50%)

---
+++ Logica/beecrowd-1929/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        int[] num = new int[4];
+        for(int i = 0; i < 4; i++){
+            num[i] = entrada.nextInt();
+        }
+
+        Arrays.sort(num);
+
+        if(num[0] == num[1] && num[0] == num[2] || num[0] == num[2] && num[0] == num[3]){
+            System.out.println("S");
+        } else if(num[0] + num[1] >= num[2] || num[0] + num[1] >= num[3]){
+            System.out.println("S");
+        }else if(num[0] + num[2] >= num[1] || num[0] + num[2] >= num[3]){
+            System.out.println("S");
+        }else{
+            System.out.println("N");
+        }
+
+        entrada.close();
+    }
+}
