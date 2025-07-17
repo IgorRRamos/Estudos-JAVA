c9734046ddd0eb31f76d66ec4c4fb26cf4faf23d
IgorRRamos <ramosigorprofissional@gmail.com>
2025-07-16 22:18:34 -0300

    Atualizaçao do codigo, com mais funcionalidades.

--- Diversos/senha/src/Main.java
+++ Diversos/senha/src/Main.java
@@ -10,6 +10,30 @@ public class Main {
         }
     }
 
+    public static void temCaracter(String senha){
+        if(senha.matches(".*[^a-zA-Z0-9 ].*")){
+            System.out.println("Ok!");
+        }else{
+            System.out.println("Need especial caracter!");
+        }
+    }
+
+    public static void temNumero(String senha){
+        if(senha.matches(".*[0-9].*")) {
+            System.out.println("Ok!");
+        }else {
+            throw new RuntimeException("Need number!");
+        }
+    }
+
+    public static void tamanhoMinimo(String senha){
+        if(senha.length() > 7){
+            System.out.println("Ok!");
+        }else{
+            throw new RuntimeException("Minimum of 8 digits!");
+        }
+    }
+
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         String senha;
@@ -18,7 +42,9 @@ public class Main {
         senha = entrada.nextLine();
 
         temMaiuscula(senha);
-
+        temCaracter(senha);
+        temNumero(senha);
+        tamanhoMinimo(senha);
 
         entrada.close();
     }
