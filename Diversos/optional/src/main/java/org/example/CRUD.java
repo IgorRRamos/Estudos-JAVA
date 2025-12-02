3bfb1ba942cf49a7a6b49707867873735b57ea13
IgorRRamos <ramosigorprofissional@gmail.com>
2025-12-02 17:17:08 -0300

    Atualização do codigo, melhorando o metodo buscaPessoa

--- Diversos/optional/src/main/java/org/example/CRUD.java
+++ Diversos/optional/src/main/java/org/example/CRUD.java
@@ -1,4 +1,24 @@
 package org.example;
 
+import java.util.List;
+import java.util.Optional;
+
 public class CRUD {
+
+    private List<Pessoa> pessoas;
+
+    public CRUD(List<Pessoa> pessoa) {
+        this.pessoas = pessoa;
+    }
+
+    public void adicionar(String nome, int idade, int id) {
+        pessoas.add(new Pessoa(111));
+    }
+
+
+    private Optional<Pessoa> buscaPessoa(int id) {
+        Optional<Pessoa> pessoa = Optional.of(new Pessoa(id));
+        return pessoas.contains(pessoa.get()) ? Optional.ofNullable(pessoas.get(pessoas.indexOf(pessoa.get()))) : Optional.empty();
+    }
+
 }
--- Diversos/optional/src/main/java/org/example/Pessoa.java
+++ Diversos/optional/src/main/java/org/example/Pessoa.java
@@ -1,4 +1,48 @@
 package org.example;
 
+import java.util.Objects;
+
 public class Pessoa {
+    private String nome;
+    private int idade, id;
+
+    public Pessoa(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return id == pessoa.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
