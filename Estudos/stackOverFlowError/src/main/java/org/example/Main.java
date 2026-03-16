64f987c3bb0c477eddbc4d6656cf4e6dc735f2a2
IgorRRamos <ramosigorprofissional@gmail.com>
2026-03-16 20:49:09 -0300

    Estudos sobre exceptions

---
+++ Estudos/stackOverFlowError/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+public class Main {
+    public static void main(String[] args) {
+        recursividade();
+    }
+    public static void recursividade(){
+        recursividade();
+    }
+}
