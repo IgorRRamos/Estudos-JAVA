7d194d19326627f7964a0bccb1909433992d513d
IgorRRamos <ramosigorprofissional@gmail.com>
2026-03-16 21:02:37 -0300

    Tratamento de exceptions checked and unChecked

---
+++ Estudos/exceptionCheckUnCheck/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+public class Main {
+    public static void main(String[] args) {
+        //Checked and UnChecked
+        Object object = null;
+
+        System.out.println(object.toString());
+
+
+    }
+}
