3edc95170e49e1a9563e9ab61c6c96c532d72860
IgorRRamos <ramosigorprofissional@gmail.com>
2026-03-17 21:18:49 -0300

    Estudo sobre tratamento de exceções.

---
+++ Estudos/exceptionTest01/src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        criarNovoArquivo();
+    }
+    public static void criarNovoArquivo() {
+        File file = new File("arquivo//teste.txt");
+        try {
+            boolean isCriado = file.createNewFile();
+            System.out.println("Arquivo criado: " + isCriado);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
